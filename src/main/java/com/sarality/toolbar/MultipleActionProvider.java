c9b3f84ddbd48fd8902d2b016b1211cdfec156d5
Abhideep Singh <abhi@dothat.in>
2019-03-14 10:21:56 +0530

    Menu that supports Click and Long click actions on same Menu Item

---
+++ src/main/java/com/sarality/toolbar/MultipleActionProvider.java
@@ -0,0 +1,65 @@
+package com.sarality.toolbar;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.view.ActionProvider;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.sarality.action.ClickActions;
+import com.sarality.action.LongClickActions;
+import com.sarality.action.ViewAction;
+
+/**
+ * Action Provider that registers the Click and Long Click Actions on a Menu Item.
+ *
+ * @author abhideep@ (Abhideep Singh)
+ */
+public class MultipleActionProvider extends ActionProvider {
+  private final Context context;
+
+  private Activity activity;
+  private MultipleActionMenu menu;
+  private MenuItem menuItem;
+  private int itemId;
+  private int itemLayoutId;
+
+  public MultipleActionProvider(Context context) {
+    super(context);
+    this.context = context;
+  }
+
+  @Override
+  public View onCreateActionView() {
+    LayoutInflater inflater = LayoutInflater.from(context);
+    View view = inflater.inflate(itemLayoutId, null);
+    initItem(view);
+    return view;
+  }
+
+  public void init(Activity activity, MultipleActionMenu menu, MenuItem menuItem, int itemId, int itemLayoutId) {
+    this.activity = activity;
+    this.menu = menu;
+    this.menuItem = menuItem;
+    this.itemId = itemId;
+    this.itemLayoutId = itemLayoutId;
+  }
+
+  private void initItem(View view) {
+    Drawable iconDrawable = menuItem.getIcon();
+    if (iconDrawable != null) {
+      view.setBackgroundDrawable(iconDrawable);
+    }
+    ViewAction clickAction = menu.getClickAction(itemId);
+    if (clickAction != null) {
+      new ClickActions(activity).register(clickAction).init(view);
+    }
+
+    ViewAction longClickAction = menu.getLongClickAction(itemId);
+    if (longClickAction != null) {
+      new LongClickActions(activity).register(longClickAction).init(view);
+    }
+  }
+}
